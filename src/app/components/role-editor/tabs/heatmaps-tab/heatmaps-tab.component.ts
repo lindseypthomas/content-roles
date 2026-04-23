ec868f758fc35f68f2857b4cc41329204ffd157f
lindseypthomas <lthomas@liaisonfs.com>
2026-04-23 16:18:14 +0100

    Add HeatmapsTabComponent with basic template

---
+++ src/app/components/role-editor/tabs/heatmaps-tab/heatmaps-tab.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heatmaps-tab',
+  imports: [],
+  template: `
+    <div class="p-4">
+      <p class="text-gray-600">Heatmaps content will be displayed here.</p>
+    </div>
+  `,
+})
+export class HeatmapsTabComponent {}
