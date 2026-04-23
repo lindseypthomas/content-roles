1e30dd6279d2da8fafcbf7dbee1394a11c28d23f
lindseypthomas <lthomas@liaisonfs.com>
2026-04-23 16:18:58 +0100

    Create metric-tiles-tab.component.ts

---
+++ src/app/components/role-editor/tabs/metric-tiles-tab/metric-tiles-tab.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-metric-tiles-tab',
+  imports: [],
+  template: `
+    <div class="p-4">
+      <p class="text-gray-600">Metric Tiles content will be displayed here.</p>
+    </div>
+  `,
+})
+export class MetricTilesTabComponent {}
